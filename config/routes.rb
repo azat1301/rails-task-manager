4a07dd572409cfbefd5bae6945987881365d91ff
Azat Allayarov <azatallayarow@gmail.com>
2022-05-10 16:55:53 +0900

    Last submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Read All
-  def index
-  end
-  # Read one
+  get 'tasks', to: 'tasks#index', as: :tasks
   # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
   # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
